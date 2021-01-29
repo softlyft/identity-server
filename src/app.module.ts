654928818ca5c1b531c91e64dbb3fc1b02a9101e
Olumide <noble2016@gmail.com>
2021-01-29 18:16:46 +0100

    create an account

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 
+import { UsersModule } from './users/users.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.DATABASE_URL)
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
