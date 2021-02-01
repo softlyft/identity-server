b448120c8680c64e2fe2b113877e56d9e04c61e8
Olumide <noble2016@gmail.com>
2021-02-01 09:58:14 +0100

    CSRF protection added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors();
+  app.use(csurf());
   await app.listen(3000);
 }
 bootstrap();
